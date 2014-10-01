9aac45a513a6dbd61746205cfbf194f46d0f63ad
Dominik Bylica <dominikjacekbylica@gmail.com>
2014-10-02 01:09:05 +0200

    Indent differently.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,38 +8,34 @@
  *
  * Main module of the application.
  */
-angular
-  .module('thoulikeApp', [
-    'ngAnimate',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-    .when('/', {
-      templateUrl: 'views/thoughts.html',
-      controller: 'ThoughtsController'
-    })
-    .when('/about', {
-      templateUrl: 'views/about.html',
-      controller: 'AboutController'
-    })
-    .when('/settings', {
-      templateUrl: 'views/settings.html',
-      controller: 'SettingsController'
-    })
-    .when('/sign_in', {
-      templateUrl: 'views/sign_in.html',
-      controller: 'SignInController'
-    })
-    .when('/confirm', {
-      templateUrl: 'views/confirm.html',
-      controller: 'ConfirmController'
-    })
-    .when('/reset', {
-      templateUrl: 'views/reset.html',
-      controller: 'ResetController'
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
+angular.module('thoulikeApp', ['ngAnimate', 'ngRoute'])
+.config(function ($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/thoughts.html',
+    controller: 'ThoughtsController'
+  })
+  .when('/about', {
+    templateUrl: 'views/about.html',
+    controller: 'AboutController'
+  })
+  .when('/settings', {
+    templateUrl: 'views/settings.html',
+    controller: 'SettingsController'
+  })
+  .when('/sign_in', {
+    templateUrl: 'views/sign_in.html',
+    controller: 'SignInController'
+  })
+  .when('/confirm', {
+    templateUrl: 'views/confirm.html',
+    controller: 'ConfirmController'
+  })
+  .when('/reset', {
+    templateUrl: 'views/reset.html',
+    controller: 'ResetController'
+  })
+  .otherwise({
+    redirectTo: '/'
   });
+});
