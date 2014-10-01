643bc8df61233efcfa3bf5312aac1a9dd94500ec
Dominik Bylica <dominikjacekbylica@gmail.com>
2014-10-02 01:10:16 +0200

    Add thoughts view and controller.

--- app/scripts/controllers/thoughts.js
+++ app/scripts/controllers/thoughts.js
@@ -7,6 +7,66 @@
  * # MainCtrl
  * Controller of the testApp
  */
+
 angular.module('thoulikeApp')
-  .controller('ThoughtsController', function ($scope) {
+.controller('ThoughtsController', ['$scope', '$http', function($scope, $http) {
+  $scope.people = [];
+  $scope.noSearches = true;
+
+  $scope.findnshare = function() {
+    $scope.noSearches = false;
+    $scope.people = [];
+    setTimeout(function() {
+      var request = $http({
+        method: 'get',
+        url: 'http://localhost:3000/thoughts/findnshare/' + $scope.thought,
+        headers: { 'X-User-Token': '-ixFszgyU8zC8vVa1Xs2', 'X-User-Email': 'dominik2cucu@gmail.com' },
+      });
+
+      return(request.then($scope.handleSuccess, $scope.handleError));
+    }, 1000);
+  };
+
+  $scope.handleSuccess = function(response) {
+    $scope.people = response.data;
+  };
+
+  $scope.handleError = function(response) {
+  };
+}]);
+
+function showing(from, to, element, done, height) {
+  jQuery(element).css({
+    opacity: from && 1,
+    height: (height ? from : undefined)
   });
+  jQuery(element).animate({
+    opacity: to && 1,
+    height: (height ? to : undefined)
+  }, 1000, done);
+}
+
+angular.module('thoulikeApp')
+.animation('.welcome', function() {
+  return {
+    enter: function(element, done) {
+      showing(0, element.css('height', 'auto').height(), element, done);
+    },
+    leave: function(element, done) {
+      showing(element.height(), 0, element, done);
+    }
+  };
+});
+
+angular.module('thoulikeApp')
+.animation('.person', function() {
+  return {
+    enter: function(element, done) {
+      showing(0, element.css('height', 'auto').height(), element, done);
+    },
+    leave: function(element, done) {
+      showing(element.height(), 0, element, done);
+    }
+  };
+});
+
